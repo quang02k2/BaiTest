314c05284a448e7c8c1c9fbe5e74a90afb85b8d1
Vu Duc Quang <VDQuang02k2@gmail.com>
2023-12-15 13:58:24 +0700

    tao

---
+++ src/test/java/com/example/BaiTest/BaiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaiTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
